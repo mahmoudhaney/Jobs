c648ce7d0db927b819eb531780ea00404fdf6876
Mahmoud Haney <mahmoudhaney25@gmail.com>
2023-10-09 15:19:08 +0300

    Prapare the Backend environment

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views
-
-app_name = 'home'
-
-urlpatterns = [
-    path('', views.home, name='home')
-]
--- job/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, Job, Candidate
-
-admin.site.register(Category)
-admin.site.register(Job)
-admin.site.register(Candidate)
