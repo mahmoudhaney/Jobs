ca9a3fd7a0468dd4a1a54cb2e1d3e1b7f8845987
Mahmoud Haney <mahmoudhaney25@gmail.com>
2023-10-10 14:04:08 +0300

    Apply Pagination, Filters, Seaching, and Ordering

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,8 @@
 from .models import Category, Job, Candidate
 from .serializers import CategorySerializer, JobSerializer, CandidateSerializer
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 # Category
 class CategoryListCreate(generics.ListCreateAPIView):
@@ -16,6 +18,10 @@ class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 class JobListCreate(generics.ListCreateAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['category', 'job_type']
+    search_fields = ['title']
+    ordering_fields = ['title', 'experience', 'salary']
 
 class JobRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Job.objects.all()
--- project/settings.py
+++ project/settings.py
@@ -25,8 +25,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'jobs',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
